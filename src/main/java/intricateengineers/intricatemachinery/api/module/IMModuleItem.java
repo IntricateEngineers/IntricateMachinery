cf2f98007161baff0e15004f4ced11fe03841913
topisani <tobiaspisani@gmail.com>
2016-05-27 16:22:10 +0200

    add a working but EXTREMELY SLOW custom renderer

--- src/main/java/intricateengineers/intricatemachinery/api/module/IMModuleItem.java
+++ src/main/java/intricateengineers/intricatemachinery/api/module/IMModuleItem.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016 IntricateEngineers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package intricateengineers.intricatemachinery.api.module;
 
 import intricateengineers.intricatemachinery.api.util.VectorUtils;
@@ -10,6 +26,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
@@ -28,7 +45,8 @@ public class IMModuleItem extends ItemMultiPart {
     public IMultipart createPart(World world, BlockPos pos, EnumFacing side, Vec3d hit, ItemStack stack, EntityPlayer player) {
         try {
             IMModule instance = module.newInstance();
-            instance.setLocalPos(VectorUtils.modulus(hit, 1));
+            byte dir = (byte) (MathHelper.floor_double((double)((player.rotationYaw * 4F) / 360F) + 0.5D) & 3);
+            instance.setLocalPos(VectorUtils.modulus(hit, 1), dir);
             return instance;
         } catch (InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
