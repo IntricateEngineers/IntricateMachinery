95edf1eb06eb2332ee64262f24b8f161198940cf
topisani <tobiaspisani@gmail.com>
2016-06-05 21:09:30 +0200

    Register modules with function to create a new module.

--- src/main/java/intricateengineers/intricatemachinery/api/module/Modules.java
+++ src/main/java/intricateengineers/intricatemachinery/api/module/Modules.java
@@ -3,20 +3,22 @@ package intricateengineers.intricatemachinery.api.module;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.base.Function;
+
 import net.minecraft.util.ResourceLocation;
 
 public class Modules {
 
-    private static Map<ResourceLocation, Class<? extends Module>> registry = new HashMap<>();
+    private static Map<ResourceLocation, Function<MachineryFrame, Module>> registry = new HashMap<>();
 
 
-    public static void registerModule(ResourceLocation name, Class<? extends Module> moduleClass) {
-        registry.put(name, moduleClass);
+    public static void registerModule(ResourceLocation name, Function<MachineryFrame, Module> newModule) {
+        registry.put(name, newModule);
     }
 
-    public static Module newModule(ResourceLocation name) {
+    public static Module newModule(ResourceLocation name, MachineryFrame frame) {
         try {
-            return registry.get(name).newInstance();
+            return registry.get(name).apply(frame);
         } catch (Exception e) {
             return null;
         }
