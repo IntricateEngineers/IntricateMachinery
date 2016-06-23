f23208d5b19c69cbaaa000c723cb61a9cecbf7c6
Nick Renieris <makren67@gmail.com>
2016-06-23 19:07:21 +0300

    Updated Forge

--- src/main/scala/intricateengineers/intricatemachinery/core/IntricateMachinery.scala
+++ src/main/scala/intricateengineers/intricatemachinery/core/IntricateMachinery.scala
@@ -34,7 +34,7 @@ import intricateengineers.intricatemachinery.common.init.ModBlocks
 @Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.MOD_VERSION, dependencies = IntricateMachinery.DEPENDENCIES, acceptableRemoteVersions = "*", modLanguage = "scala")
 object IntricateMachinery {
 
-    final val DEPENDENCIES = "required-after:Forge@[" + "12.17.0.1954" + ",)after:mcmultipart"
+    final val DEPENDENCIES = "required-after:Forge@[" + "12.17.0.1980" + ",);after:mcmultipart@[1.0.8,)"
 
     @Instance(ModInfo.MOD_NAME)
     var instance = this
@@ -46,7 +46,7 @@ object IntricateMachinery {
     def preInit(event : FMLPreInitializationEvent) {
         ConfigIM.config = new Configuration(new File(event.getModConfigurationDirectory, ModInfo.MOD_ID + ".cfg"))
         ModBlocks.init()
-
+        println(DEPENDENCIES)
         MinecraftForge.EVENT_BUS.register(new EventManager)
         MinecraftForge.EVENT_BUS.register(proxy)
         proxy.preInit(event)
