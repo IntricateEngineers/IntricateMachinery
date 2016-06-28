2d248b701a787d40c290b55a22c17f545cf2d92b
topisani <tobiaspisani@gmail.com>
2016-06-28 02:16:23 +0200

    Added forEachCoord to  ModuleList

--- src/main/scala/intricateengineers/intricatemachinery/api/module/MachineryFrame.scala
+++ src/main/scala/intricateengineers/intricatemachinery/api/module/MachineryFrame.scala
@@ -211,14 +211,30 @@ class ModuleList extends Traversable[Module] {
   }
 
   def forEachCoord(m: Module, f: (Box, BoxFace, Int, Int, Int) ⇒ Unit): Unit = {
-    for (b ← m.model.boxes)
+    for (b ← m.boxCache())
+      forEachCoord(b, (face, x, y, z) => f(b, face, x, y, z))
+  }
+
+  def forEachCoord(b: Box, f: (BoxFace, Int, Int, Int) => Unit): Unit = {
       for (face ← b.faces) {
         val (from, to) = b.vecs(face)
         for (x ← from.x.toInt until to.x.toInt;
              y ← from.y.toInt until to.y.toInt;
              z ← from.z.toInt until to.z.toInt)
-          f(b, face, x, y, z)
+          f(face, x, y, z)
       }
+  }
+
+  def forEachAdjecant(b: Box, f: (BoxFace, Int, Int, Int) => Unit): Unit = {
+    forEachCoord(b, (face, x, y, z) => {
+      val offset = face.side.getDirectionVec
+      f(face, x + offset.getX, y + offset.getY, z + offset.getZ)
+    })
+  }
+
+  def forEachAdjecant(m: Module, f: (Box, BoxFace, Int, Int, Int) => Unit): Unit = {
+    for (b <- m.boxCache())
+      forEachAdjecant(b, (face, x, y, z) => f(b, face, x, y, z))
   }
 
   override def foreach[U](f: (Module) => U) = modules.foreach(f)
